8d44cc036857df37e32516c04f7808a5390a7db1
Eric Tsai <hiphop200199@gmail.com>
2023-03-29 10:12:37 +0800

    建構scss

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 
 <template>
   <nav class="navbar">
-    <router-link to="/" >Home</router-link>
-  <router-link to="/about" >About</router-link>
+    <router-link class="link" to="/" >Home</router-link>
+    <router-link class="link" to="/about" >About</router-link>
     </nav>
   <router-view/>
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,65 @@
 <script setup>
+/*import { ref } from 'vue';
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd7f3e04d0fmsh481d6b03cd7494ep14cfdcjsn8775ef35e2c3',
+		'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
+	}
+};
+
+const data=ref([]);
+
+async function getSymbols(){
+    const res = await fetch(
+    "https://currency-conversion-and-exchange-rates.p.rapidapi.com/symbols",
+    options
+  );
+
+  const finalRes = await res.json();
+
+  data.value=finalRes;
+}
+
+getSymbols();*/
 </script>
 
 <template>
   <div class="container">
     <h1 class="title">The Currency.</h1>
     <h2 class="subtitle">information about exchange rates.</h2>
-    
+    <section class="function-area">
+        <section class="function">
+            <h3 class="function-title">latest rate</h3>
+            <label for="">from: </label>
+            <input type="text" name="" id="">
+            <label for="">to: </label>
+            <input type="text" name="" id="">
+            <button>send</button>
+            <p class="result">result: </p>
+        </section>
+        <section class="function">
+            <h3 class="function-title">historical rate</h3>
+            <label for="">the date:</label>
+            <input type="date" name="" id="">
+            <label for="">from: </label>
+            <input type="text" name="" id="">
+            <label for="">to: </label>
+            <input type="text" name="" id="">
+            <button>send</button>
+            <p class="result">result: </p>
+        </section>
+        <section class="function">
+            <h3 class="function-title">convert</h3>
+            <label for="">from: </label>
+            <input type="text" name="" id="">
+            <label for="">to: </label>
+            <input type="text" name="" id="">
+            <button>send</button>
+            <p class="result">result: </p>
+        </section>
+    </section>
   </div>
 </template>
 
