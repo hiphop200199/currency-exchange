1a8a5545e8d24e54898f1941712873721bcccd60
Eric Tsai <hiphop200199@gmail.com>
2023-04-01 23:15:07 +0800

    file uploads

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,9 @@ import About from '../components/About.vue'
 import Reference from '../components/Reference.vue'
 
 const routes = [
-  {path: "/", name: "Home", component: Home},
-  {path: "/about", name: "About", component: About},
-  {path:"/reference",name:"Reference",component:Reference},
+  {path: "/currency-exchange/", name: "Home", component: Home},
+  {path: "/currency-exchange/about", name: "About", component: About},
+  {path:"/currency-exchange/reference",name:"Reference",component:Reference},
   
 ]
 
