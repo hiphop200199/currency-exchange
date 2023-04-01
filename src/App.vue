0677409ad1f0601f3736b8ea4d7db192b6f63f40
Eric Tsai <hiphop200199@gmail.com>
2023-04-01 23:35:17 +0800

    file uploads

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
 
 <template>
   <nav class="navbar">
-    <router-link class="link" to="/" >Home</router-link>
-    <router-link class="link" to="/about" >About</router-link>
-    <router-link class="link" to="/reference" >Reference</router-link>
+    <router-link class="link" to="/currency-exchange/" >Home</router-link>
+    <router-link class="link" to="/currency-exchange/about" >About</router-link>
+    <router-link class="link" to="/currency-exchange/reference" >Reference</router-link>
     </nav>
   <router-view/>
 </template>
