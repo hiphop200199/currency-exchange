368d8b16901cf3eeb198c1adb532f4534aaf37a0
Eric Tsai <hiphop200199@gmail.com>
2023-04-01 22:23:24 +0800

    file uploads

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     <router-link class="link" to="/" >Home</router-link>
     <router-link class="link" to="/about" >About</router-link>
     <router-link class="link" to="/reference" >Reference</router-link>
+    <router-link class="link" to="*" >Home</router-link>
     </nav>
   <router-view/>
 </template>
